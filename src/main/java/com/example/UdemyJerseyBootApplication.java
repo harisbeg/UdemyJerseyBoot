1c3ac84102fbab78971f0f81efd5dc957678528d
harisbeg <haris.beg@hotmail.com>
2016-03-15 18:34:28 -0700

    Converted the Application class into a Controller

--- src/main/java/com/example/UdemyJerseyBootApplication.java
+++ src/main/java/com/example/UdemyJerseyBootApplication.java
@@ -2,11 +2,22 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
+@RequestMapping("/hello")
 public class UdemyJerseyBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UdemyJerseyBootApplication.class, args);
+	}
+	
+	@RequestMapping("")
+	@ResponseBody
+	public String hello() {
+		return "Hello World!";
 	}
 }
